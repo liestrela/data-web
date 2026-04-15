b79de5ed4dc500bf85d6a4c6c149634f0902c35a
vinivid <frato.vini@gmail.com>
2026-04-14 21:22:56 -0300

    Css nav

--- src/routes/MainPage.tsx
+++ src/routes/MainPage.tsx
@@ -1,10 +1,17 @@
 import { ReviewManager } from "../components/ReviewManager";
+import { Link } from "react-router-dom";
 
 export function MainPage() {
 	return (
-		<div className="app">
-			<h1>Calendário de Revisões</h1>
-			<ReviewManager />
+		<div style={{width: '100%'}}>
+			<nav className="nav_user">
+				<Link to="/login">Login</Link>
+				<Link to="/register">Registrar</Link>
+			</nav>
+			<div className="app">
+				<h1>Calendário de Revisões</h1>
+				<ReviewManager />
+			</div>
 		</div>
 	);
 }
