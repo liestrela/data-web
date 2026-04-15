6ce1e884c37d3b0c87a19c9de79d545995c0c69e
vinivid <frato.vini@gmail.com>
2026-04-14 21:53:33 -0300

    Seção do usuario

--- src/routes/MainPage.tsx
+++ src/routes/MainPage.tsx
@@ -1,13 +1,40 @@
 import { ReviewManager } from "../components/ReviewManager";
 import { Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import defaultUserImage from '../assets/default.jpg'
+import { useState } from "react";
 
 export function MainPage() {
+  const { user, logout } = useAuth();
+  const [isUserOpen, setIsUserOpen] = useState(false);
+
 	return (
 		<div style={{width: '100%'}}>
-			<nav className="nav_user">
-				<Link to="/login">Login</Link>
-				<Link to="/register">Registrar</Link>
-			</nav>
+
+			{!user ? (
+				<nav className="nav_user">
+					<Link to="/login">Login</Link>
+					<Link to="/register">Registrar</Link>
+				</nav>
+			) : (
+				<nav className="nav_user">
+          <img 
+            src={defaultUserImage} 
+            alt="Imagem do usuário."
+            onClick={() => setIsUserOpen(!isUserOpen)} 
+          />
+
+          {isUserOpen && (
+            <div className="user_drop">
+              <ul>
+                <li onClick={logout}>Logout</li>
+              </ul>
+            </div>
+            )
+          }
+				</nav>
+			)}
+
 			<div className="app">
 				<h1>Calendário de Revisões</h1>
 				<ReviewManager />
